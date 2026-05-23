3f74890a762558b64a77197f787839a1551cde34
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-23 13:13:23 +0530

    Time: 0 ms (100%), Space: 49.4 MB (66.83%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = countl(root);
+        int right = countr(root);
+
+        if (left == right) {
+            return (2 << left) - 1;
+        }
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+
+    public int countl(TreeNode root) {
+        int count = 0;
+
+        while (root.left != null) {
+            count++;
+            root = root.left;
+        }
+
+        return count;
+    }
+
+    public int countr(TreeNode root) {
+        int count = 0;
+
+        while (root.right != null) {
+            count++;
+            root = root.right;
+        }
+
+        return count;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
