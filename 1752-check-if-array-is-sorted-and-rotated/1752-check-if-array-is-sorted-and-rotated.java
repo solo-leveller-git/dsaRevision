1339eebf35ab4176d8220944a5c26dbf361fa29b
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-23 09:45:59 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.14%) - LeetHub - Time: O(?), Space: O(?)

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean check(int[] nums) {
 
-        int min = nums[0];
         int n = nums.length - 1;
 
-        while (n >= 0 && nums[0] == nums[n]) n--;
+        while (n >= 0 && nums[0] == nums[n]) 
+            n--;
 
         int i = 1;
 
-        while (i <= n && nums[i] >= nums[i - 1]) i++;
-
+        while (i <= n && nums[i] >= nums[i - 1]) 
+            i++;
         if (i > n) 
             return true;
 
         for (int j = i + 1; j <= n; j++) {
-            if (nums[j] < nums[j - 1] || nums[j] >= min)
+            if (nums[j] < nums[j - 1] || nums[j] > nums[0])
                 return false;
         }
 
-     if (nums[n] > nums[0])
+        if (nums[n] > nums[0])
             return false;
 
         return true;
     }
 }
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
