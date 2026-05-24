180eecbaf63a9e28431b37d1d433fa7c11429de3
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-24 22:13:39 +0530

    Added solution - LeetHub

---
+++ 705589-two-odd-occuring-GFG/705589-two-odd-occuring.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    ArrayList<Integer> twoOddNum(int[] arr) {
+
+        int xor = 0;
+
+        for (int n : arr) {
+            xor ^= n;
+        }
+
+        int mask = xor & -xor;
+
+        int first = 0;
+        int second = 0;
+
+        for (int n : arr) {
+
+            if ((n & mask) == 0) {
+                first ^= n;
+            } else {
+                second ^= n;
+            }
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if (first > second) {
+            ans.add(first);
+            ans.add(second);
+        } else {
+            ans.add(second);
+            ans.add(first);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
