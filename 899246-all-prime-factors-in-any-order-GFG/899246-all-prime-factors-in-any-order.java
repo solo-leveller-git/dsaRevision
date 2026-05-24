45120f11470a154b67deb18a84ca983ace32ddc7
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-24 22:09:07 +0530

    Added solution - LeetHub

---
+++ 899246-all-prime-factors-in-any-order-GFG/899246-all-prime-factors-in-any-order.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    ArrayList<Integer> primeFactors(int n) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        while (n % 2 == 0) {
+            ans.add(2);
+            n /= 2;
+        }
+
+        for (int i = 3; i * i <= n; i += 2) {
+
+            while (n % i == 0) {
+                ans.add(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1) {
+            ans.add(n);
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
