3b7d1cfcefa5537158024e4bd77e955d955c1a6d
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-24 21:45:43 +0530

    Time: 1 ms (100%), Space: 48 MB (58.79%) - LeetHub - Time: O(n), Space: O(1)

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,32 +1,23 @@
 class Solution {
-
     public int[] singleNumber(int[] nums) {
-
         int totalXor = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            totalXor ^= nums[i];
+        for(int i = 1 ; i < nums.length ; i++){
+            totalXor^=nums[i];
         }
-
         int j = 0;
-
-        while (((totalXor >> j) & 1) != 1) {
+        while((totalXor&1)!=1){
+            totalXor>>=1;
             j++;
         }
-
         int first = 0;
         int second = 0;
+        for(int n : nums){
+           int temp = n;
+           if(((temp>>j)&1)==0)first^=n;
+           else second^=n;
 
-        for (int n : nums) {
-
-            if (((n >> j) & 1) == 0) {
-                first ^= n;
-            } else {
-                second ^= n;
-            }
         }
-
-        return new int[]{first, second};
+        return new int[]{first,second};
     }
 }
 
