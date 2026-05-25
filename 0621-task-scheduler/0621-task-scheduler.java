d6c2eb8a7a378025676aeecc3562ed1345249084
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-25 15:31:14 +0530

    Time: 31 ms (26.53%), Space: 46 MB (99.99%) - LeetHub - Time: O(?), Space: O(?)

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,55 +1,29 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-
-        Map<Character,Integer> m = new HashMap<>();
-
-        for(char ch : tasks){
-            m.put(ch, m.getOrDefault(ch,0) + 1);
+        Map<Character,Integer> fmap = new HashMap<>();
+        for(char t:tasks){
+            fmap.put(t,fmap.getOrDefault(t,0)+1);
         }
-
-        PriorityQueue<Integer> pq =
-            new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int v : m.values()){
-            pq.add(v);
-        }
-
-        int ans = 0;
-
-        while(!pq.isEmpty()){
-
-            List<Integer> l = new ArrayList<>();
-
-            int count = 0;
-
-            for(int i = 0 ; i < n + 1 ; i++){
-
-                if(!pq.isEmpty()){
-
-                    int out = pq.poll();
-
-                    if(out - 1 > 0){
-                        l.add(out - 1);
-                    }
-
-                    count++;
+        PriorityQueue<Integer> p = new PriorityQueue<>(Collections.reverseOrder());
+        p.addAll(fmap.values());
+        Queue<int[]> q = new LinkedList<>();
+        int time = 0;
+         while(!q.isEmpty() || !p.isEmpty()){
+            time++;
+            if(!q.isEmpty() && q.peek()[0]==time){
+                p.offer(q.poll()[1]);
+            }
+            if(!p.isEmpty()){
+                int f = p.poll();
+                int rf = f-1;
+                if(rf>0)
+                {
+                    int nat = time+n+1;
+                    q.add(new int[]{nat,rf});
                 }
             }
-
-            for(int x : l){
-                pq.offer(x);
-            }
-
-            if(pq.isEmpty()){
-                ans += count;
-            }
-            else{
-                ans += n + 1;
-            }
-        }
-
-        return ans;
-    }
+         }
+ return time;   }
 }
 
 // Synced seamlessly with LeetHub Pro
