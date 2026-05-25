a6e83a17362108337b8d8cb3d4d5d4114ef534c4
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-25 18:49:02 +0530

    Added solution - LeetHub

---
+++ 701365-fractional-knapsack-GFG/701365-fractional-knapsack.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    class Item {
+        int val;
+        int wt;
+        double ratio;
+
+        Item(int val, int wt) {
+            this.val = val;
+            this.wt = wt;
+            this.ratio = (double) val / wt;
+        }
+    }
+
+    public double fractionalKnapsack(int[] val, int[] wt, int capacity) {
+
+        int n = val.length;
+
+        Item[] arr = new Item[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = new Item(val[i], wt[i]);
+        }
+
+        Arrays.sort(arr, (a,b) ->
+            Double.compare(b.ratio, a.ratio));
+
+        double ans = 0;
+
+        for(Item item : arr) {
+
+            if(capacity >= item.wt) {
+
+                ans += item.val;
+                capacity -= item.wt;
+            }
+            else {
+
+                ans += item.ratio * capacity;
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
