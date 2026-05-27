cb1c52e3abf0274008e2124abc94adfc59239120
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-27 20:14:47 +0530

    Time: 1280 ms (5%), Space: 60.6 MB (5.4%) - LeetHub - Time: O(?), Space: O(?)

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,19 +1,22 @@
 class Solution {
     public int countSubstrings(String s) {
+        Boolean[][] dp = new Boolean[s.length()][s.length()];
+
         int count = 0;
         for(int i = 0 ; i < s.length() ; i++){
             for(int j = i ; j < s.length() ; j++){
-                if(isPal(s,i,j)){
+                if(isPal(s,i,j,dp)){
                     count++;
                 }
             }
         }
         return count;
     }
-    public boolean isPal(String s, int i, int j){
+    public boolean isPal(String s, int i, int j, Boolean[][] dp){
         if(i>=j)return true;
+        if(dp[i][j]!=null)return dp[i][j];
         if(s.charAt(i)!=s.charAt(j))return false;
-        return isPal(s,i+1,j-1);
+        return isPal(s,i+1,j-1,dp);
     }
 }
 
