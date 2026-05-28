26a8c12ff8bf61209e51959812a41b8f24749958
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-28 14:31:25 +0530

    Time: 0 ms (100%), Space: 43.8 MB (69.32%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode last = null;
+    public void flatten(TreeNode root) {
+        if(root==null)return;
+        flatten(root.right);
+        flatten(root.left);
+        root.left=null;
+        root.right=last;
+        last=root;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
