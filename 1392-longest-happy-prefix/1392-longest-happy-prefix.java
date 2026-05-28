b06d5ff5f48a32e0d8be16deb6a3aa60c03d2ee7
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-28 22:46:00 +0530

    Time: 9 ms (94.21%), Space: 47.5 MB (77.23%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] lps = new int[n];
+
+        int len = 0; 
+        int i = 1;
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        int happyLength = lps[n - 1];
+
+        return s.substring(0, happyLength);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
