4e96bdb0f7c867847f8f516048766006822cc873
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-30 14:13:09 +0530

    Time: 24 ms (74.72%), Space: 80.9 MB (87.09%) - LeetHub - Time: O(?), Space: O(?)

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,42 +1,38 @@
 class Solution {
+    int[] temp;
+
     public int[] sortArray(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
+        temp = new int[nums.length];
+        mergeSort(nums, 0, nums.length - 1);
         return nums;
     }
-    public void mergeSort(int[] nums, int l, int r){
-        if(l>=r)return;
-        int m = l+(r-l)/2;
-        mergeSort(nums,l,m);
-        mergeSort(nums,m+1,r);
-        merge(nums,l,m,r);
 
+    private void mergeSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+
+        int m = l + (r - l) / 2;
+
+        mergeSort(nums, l, m);
+        mergeSort(nums, m + 1, r);
+        merge(nums, l, m, r);
     }
-    public void merge(int[] nums, int l, int m, int r){
-        List<Integer> temp = new ArrayList<>();
-        int left = l;
-        int right = m+1;
-        while(left<=m && right<=r){
-            if(nums[left]<=nums[right]){
-            temp.add(nums[left]);
-            left++;
-            }
-            else{
-                temp.add(nums[right]);
-                right++;
-            }
-        }
-        while(left<=m){
-            temp.add(nums[left]);
-            left++;
-        }
-        while(right<=r){
-                temp.add(nums[right]);
-                right++;
-                    }
-        for(int i = l ; i <=r ; i++){
-            nums[i]=temp.get(i-l);
+
+    private void merge(int[] nums, int l, int m, int r) {
+        int left = l, right = m + 1, idx = l;
+
+        while (left <= m && right <= r) {
+            if (nums[left] <= nums[right])
+                temp[idx++] = nums[left++];
+            else
+                temp[idx++] = nums[right++];
         }
 
+        while (left <= m) temp[idx++] = nums[left++];
+        while (right <= r) temp[idx++] = nums[right++];
+
+        for (int i = l; i <= r; i++) {
+            nums[i] = temp[i];
+        }
     }
 }
 
