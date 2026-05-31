ea6a2d01feba1dd9e822432d8236cc7c373f49dc
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-05-31 16:41:37 +0530

    Added solution - LeetHub

---
+++ 700148-insertion-sort-GFG/700148-insertion-sort.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void insertionSort(int arr[]) {
+        rec(arr, arr.length);
+    }
+
+    public void rec(int arr[], int n) {
+        if (n <= 1) return;
+
+        rec(arr, n - 1);
+
+        int key = arr[n - 1];
+        int j = n - 2;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
