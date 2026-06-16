25ccd0077b94c024a053bb9c33e94c7bf918a06b
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-16 10:01:53 +0530

    Added solution - LeetHub

---
+++ 706297-construct-list-using-xor-queries-GFG/706297-construct-list-using-xor-queries.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ArrayList<Integer> constructList(int[][] queries) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int xor = 0;
+
+        for (int i = queries.length - 1; i >= 0; i--) {
+            int type = queries[i][0];
+            int val = queries[i][1];
+
+            if (type == 0) {
+                ans.add(val ^ xor);
+            } else {
+                xor ^= val;
+            }
+        }
+
+        ans.add(xor); 
+        Collections.sort(ans);
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
