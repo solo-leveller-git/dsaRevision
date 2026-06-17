9540b873d8fff8ae5642b3c2fd2d014f00b4ec3c
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-17 14:47:19 +0530

    Time: 1 ms (88.82%), Space: 43.9 MB (87.15%) - LeetHub - Time: O(2^n * n), Space: O(2^n * n)

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,31 +1,25 @@
-import java.util.*;
-
 class Solution {
-
-    List<List<Integer>> ans = new ArrayList<>();
-
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
 
-        rec(nums, 0, new ArrayList<>());
+        int n = 1 << nums.length;   
+
+        for (int i = 0; i < n; i++) {
+            List<Integer> l = new ArrayList<>();
+
+            for (int j = 0; j < nums.length; j++) {
+                if ((i & (1 << j)) != 0) {
+                    l.add(nums[j]);
+                }
+            }
+
+            ans.add(l);
+        }
 
         return ans;
-    }
-
-    public void rec(int[] n, int i, List<Integer> l) {
-
-        if(i == n.length) {
-            ans.add(new ArrayList<>(l));
-            return;
-        }
-        l.add(n[i]);
-
-        rec(n, i + 1, l);
-
-        l.remove(l.size() - 1);
-        rec(n, i + 1, l);
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
