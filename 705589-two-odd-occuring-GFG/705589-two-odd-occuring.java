811edd6f73076bd72c58027745e110f516b350dd
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-17 17:39:57 +0530

    Added solution - LeetHub

--- 705589-two-odd-occuring-GFG/705589-two-odd-occuring.java
+++ 705589-two-odd-occuring-GFG/705589-two-odd-occuring.java
@@ -1,41 +1,32 @@
 class Solution {
-
-    ArrayList<Integer> twoOddNum(int[] arr) {
-
-        int xor = 0;
-
-        for (int n : arr) {
-            xor ^= n;
-        }
-
-        int mask = xor & -xor;
-
+    ArrayList<Integer> twoOddNum(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int totalXor = 0;
+        for(int n:nums)totalXor^=n;
+        int x = totalXor&-totalXor;
         int first = 0;
         int second = 0;
-
-        for (int n : arr) {
-
-            if ((n & mask) == 0) {
-                first ^= n;
-            } else {
-                second ^= n;
-            }
+        for(int n:nums){
+            if((n&x)>0)first^=n;
+            else second^=n;
         }
-
-        ArrayList<Integer> ans = new ArrayList<>();
-
-        if (first > second) {
-            ans.add(first);
-            ans.add(second);
-        } else {
+        if(first>second)
+        {
+            
+        ans.add(first);
+        ans.add(second);
+        }
+        else{
             ans.add(second);
             ans.add(first);
         }
-
         return ans;
+        
+        
     }
 }
 
+
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
