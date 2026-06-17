edc4010690656494b6413e6f5b4b5cb87db405d9
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-17 17:34:32 +0530

    Time: 1 ms (100%), Space: 47.8 MB (89.1%) - LeetHub - Time: O(n), Space: O(1)

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,32 +1,19 @@
 class Solution {
-
     public int[] singleNumber(int[] nums) {
-
-        int xor = 0;
-
-        for (int n : nums) {
-            xor ^= n;
-        }
-
-        // rightmost set bit
-        int mask = xor & -xor;
-
+        if(nums.length==2)return nums;
+        int totalXor = 0;
+        for(int n:nums)totalXor^=n;
+        int x = totalXor&-totalXor;
         int first = 0;
         int second = 0;
-
-        for (int n : nums) {
-
-            if ((n & mask) == 0) {
-                first ^= n;
-            } else {
-                second ^= n;
-            }
+        for(int n:nums){
+            if((n&x)>0)first^=n;
+            else second^=n;
         }
-
-        return new int[]{first, second};
+        return new int[]{first,second};
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
