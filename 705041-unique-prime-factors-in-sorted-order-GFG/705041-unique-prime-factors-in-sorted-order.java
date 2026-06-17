3f2ee39dc1774ff97e043782a49eb396aad01e29
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-17 17:52:24 +0530

    Added solution - LeetHub

---
+++ 705041-unique-prime-factors-in-sorted-order-GFG/705041-unique-prime-factors-in-sorted-order.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static ArrayList<Integer> primeFac(int n) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                ans.add(i);     
+
+                while (n % i == 0) {
+                    n /= i;      
+                }
+            }
+        }
+
+        if (n > 1) ans.add(n);
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
