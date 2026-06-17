5157f67f090105883c07976a66e8625c30c69c9c
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-17 17:05:52 +0530

    Added solution - LeetHub

--- 712176--GFG/712176-.java
+++ 712176--GFG/712176-.java
@@ -1,17 +1,18 @@
 class Solution {
-   static int oton(int n){
-        if(n%4==1)return 1;
-        else if(n%4==2)return n+1;
-        else if(n%4==3)return 0;
-        else return n;
-    }
     public static int findXOR(int l, int r) {
-int firstGroup = oton(l-1);
-int secondGroup = oton(r);
-return firstGroup^secondGroup;
+        int right = chk(r);
+        int left = chk(l-1);
+        return left^right;
+        
+    }
+    public static int chk(int n){
+       if(n % 4 == 0) return n;
+else if(n % 4 == 1) return 1;
+else if(n % 4 == 2 )return n + 1;
+else return 0;
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
