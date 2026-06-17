1ba60d89c05773484a5ae1e140050b538ebb0d20
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-17 18:07:24 +0530

    Time: 97 ms (70.16%), Space: 51.2 MB (52.93%) - LeetHub - Time: O(n log log n), Space: O(n)

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,30 +1,30 @@
 class Solution {
-
     public int countPrimes(int n) {
+        if (n <= 2) return 0;
 
-        boolean[] nonPrime = new boolean[n];
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
 
-        int ans = 0;
+        prime[0] = false;
+        prime[1] = false;
 
-        for (int i = 2; i < n; i++) {
-
-            if (!nonPrime[i]) {
-
-                ans++;
-
-                if ((long)i * i < n) {
-
-                    for (int j = i * i; j < n; j += i) {
-                        nonPrime[j] = true;
-                    }
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
                 }
             }
         }
 
-        return ans;
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) count++;
+        }
+
+        return count;
     }
 }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
