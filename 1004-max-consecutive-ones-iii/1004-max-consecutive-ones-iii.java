549a3c24301d7c476a1e120b2d92b0a96966a8c5
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-06-20 22:13:20 +0530

    Time: 4 ms (46.56%), Space: 52.5 MB (6.7%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int l = 0;
+        int r = 0;
+        int c = k;
+        int ans = 0;
+        if(k==0){
+            int sum = 0;
+            for(int i = 0 ; i < nums.length ; i++){
+                if(nums[i]==0)sum = 0;
+                else sum+=nums[i];
+                ans = Math.max(ans,sum);
+            }
+            return ans;
+        }
+        while(r<nums.length){
+            while(nums[r]==0 && c==0 && l<r){
+                if(nums[l]==0)c++;
+                l++;
+            }
+            if(nums[r]==0)c--;
+            ans=Math.max(ans,r-l+1);
+            r++;
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
