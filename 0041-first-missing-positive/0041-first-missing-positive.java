d064a40568f0112a7bda4b0d7d5012b125bc8c47
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-07-03 22:36:08 +0530

    Time: 1 ms (100%), Space: 71.2 MB (72.25%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,17 +1,20 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-     int[] bucket = new int[100001];
-     for(int n:nums){
-        if(n>=0 && n<=100000)
-        bucket[n]=1;
-     }   
-     int i = 0;
-     for(int n:bucket){
-        if(i!=0)
-        if(n==0)return i;
-        i++;
-     }
-     return 100001;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+
+        return n + 1;
     }
 }
 
