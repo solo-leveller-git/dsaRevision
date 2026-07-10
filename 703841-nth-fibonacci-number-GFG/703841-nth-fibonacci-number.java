34beb3b1772705fc97f8875884bda44564208fe9
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-07-10 20:23:05 +0530

    Added solution - LeetHub

--- 703841-nth-fibonacci-number-GFG/703841-nth-fibonacci-number.java
+++ 703841-nth-fibonacci-number-GFG/703841-nth-fibonacci-number.java
@@ -3,15 +3,7 @@ class Solution {
         if(n==0)return 0;
         if(n==1)return 1;
         
-        int a = 0;
-        int b = 1;
-        for(int i = 2; i <= n; i++){
-            int t = b;
-            b=a+b;
-            a=t;
-        }
-        return b;
-        
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
     }
 }
 
