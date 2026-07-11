3d9e0265b0913e68475e1236b2019fb56037e0ec
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-07-11 22:22:15 +0530

    Added solution - LeetHub

---
+++ 701210-array-leaders-GFG/701210-array-leaders.java
@@ -0,0 +1,25 @@
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int check = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i == arr.length - 1) {
+                ans.add(arr[i]);
+            } else {
+                if (arr[i] >= check) {
+                    check = arr[i];
+                    ans.add(check);
+                }
+            }
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
