7b60fa13a2bfa9d9c220d3c7bdd8c12428bab45d
ADITYA RAJ <2023041109@mmmut.ac.in>
2026-07-13 16:41:21 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.2%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target)start=mid+1;
+            else end=mid-1;
+        }
+        return -1;
+     }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
